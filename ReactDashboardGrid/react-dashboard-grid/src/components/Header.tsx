cdc1c14d9c05d3c6536eb4cb609928c0dbaa898f
yiannisfaf <yiannisfaf@hotmail.com>
2022-10-16 18:24:04 +0100

    initial

---
+++ ReactDashboardGrid/react-dashboard-grid/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <section className="header">
+      <div className="logo">Logo</div>
+    </section>
+  );
+}
+
+export default Header;
